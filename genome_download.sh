1d6f35dc5a65bf3d42cb939773fd33e29b0505a5
Ubuntu <ubuntu@ip-172-31-11-198.us-east-2.compute.internal>
2020-08-13 08:44:56 +0000

    Add where to download in genome_download.sh

--- genome_download.sh
+++ genome_download.sh
@@ -4,7 +4,7 @@
 cat Mycoplasma_genomes.csv | awk -F ',' '{print $15}' | tail -n +2 | sed 's/ftp:/rsync:/g' > links.txt
 
 #call rsync to download genomes
-while read p; do rsync --copy-links --recursive --times --verbose $p; done < links.txt
+while read p; do rsync --copy-links --recursive --times --verbose $p ./; done < links.txt
 
 #retrieve gbff files and unzip them
 mv */*.gbff.gz ./
